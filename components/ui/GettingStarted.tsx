61f6c6b67350ba8009899cb567c72c7aa75e14dd
Arav menon <aravmenon.ak@gmail.com>
2025-10-29 19:10:32 +0530

    setup redirecting links to whatsapp

--- components/ui/GettingStarted.tsx
+++ components/ui/GettingStarted.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 export default function GettingStarted({
   category,
@@ -9,15 +7,27 @@ export default function GettingStarted({
   category: string;
   plan: string;
 }) {
-  const router = useRouter();
+  // Create a properly encoded WhatsApp message
+  const message = `Hi Advona 👋, I’m interested in the ${category.replace(
+    /-/g,
+    " "
+  )} — ${plan} plan. Could you please share more details?`;
+
+  const whatsappURL = `https://api.whatsapp.com/send?phone=919241392799&text=${encodeURIComponent(
+    message
+  )}`;
 
   return (
-    <Link href={`/checkout/${category}/${plan}`}>
-    <button
-      className={`w-full py-2.5 md:py-3 px-4 rounded-full text-sm md:text-base cursor-pointer font-medium transition-all duration-200 bg-white/10 hover:bg-white/20 text-white shadow-md shadow-black/20 hover:shadow-lg hover:shadow-black/30 active:scale-95 active:shadow-sm active:shadow-black/10`}
+    <a
+      href={whatsappURL}
+      target="_blank"
+      rel="noopener noreferrer"
     >
-      Get Started
-    </button>
-    </Link>
+      <button
+        className="w-full py-2.5 md:py-3 px-4 rounded-full text-sm md:text-base cursor-pointer font-medium transition-all duration-200 bg-white/10 hover:bg-white/20 text-white shadow-md shadow-black/20 hover:shadow-lg hover:shadow-black/30 active:scale-95 active:shadow-sm active:shadow-black/10"
+      >
+        Let's Connect
+      </button>
+    </a>
   );
 }
